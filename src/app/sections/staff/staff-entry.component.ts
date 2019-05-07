c7bcf9562d8319d499ffd5c58f00077148849da6
Matthew Toll <matttoll@masterpeaceltd.com>
2019-05-06 21:22:26 -0400

    development on staff entry.  Building out a list of staff

--- src/app/sections/staff/staff-entry.component.ts
+++ src/app/sections/staff/staff-entry.component.ts
@@ -12,18 +12,73 @@ import { GeneralDataService } from '../../services/general-data.service';
 })
 export class StaffEntryComponent  {
 
+  staffList: Array<any>;
+  currentStaff: any;
 
   constructor( public dialog: MatDialog) {
-
+      this.staffList = [];
+      this.currentStaff = { "name": "", "skills": [] };
   }
 
   ngOnInit(): void {
-
+      this.loadMockStaff();
   }
 
-  onEdit() {
-        const dialogRef = this.dialog.open(ExpertiseDialogComponent, { width: '200', height: '300'});
+  onBlur() {
+    this.addNewStaff(this.currentStaff.name);
+  }
+
+  onKeyup(event) {
+    if (event.key === "Enter") {
+        this.addNewStaff(this.currentStaff.name);
     }
+  }
 
+  isNameUnique(name: string): boolean {
+    let item = _.find(this.staffList, (entry:any) => {
+        return entry.name === name;
+    });
+    if (typeof item === 'undefined') {
+        return true;
+    } else {
+      return false;
+    }
+  }
 
+  addNewStaff(name: string): void {
+      if (name.length > 0  && this.isNameUnique(name)) {
+        let newItem = _.cloneDeep(this.currentStaff);
+        this.staffList.unshift(newItem);
+        this.currentStaff ={ "name": "", "skills": [] };
+      }
+  }
+
+  loadMockStaff() {
+      this.staffList.push({"name": "Douglas Mayhew", "skills": []});
+      this.staffList.push({"name": "Susan Smith", "skills": []});
+      let skills = [
+            "Cyber Security Audit",
+            "Data Security",
+            "Inspection Procedures",
+            "E-Commerce | Payment Systems",
+            "E-Commerce | Product Databases",
+            "HIPPA Knowledge",
+            "Red Team | Social Engineering",
+            "Red Team | Penetration Operations",
+            "Red Team | Penetration Audit",
+            "Risk Assessment",
+            "Software Development Process",
+            "Software Specification Analysis",
+            "Systems Analysis",
+            "Testing | Performance",
+            "Testing | Load",
+            "Testing | Stress",
+            "XY Flava Triad",
+            "Zappa Kappa Doo"
+    ];
+    this.staffList.push({"name": "Douglas Mayhew", "skills": [skills[0], skills[5], skills[6]]});
+    this.staffList.push({"name": "Susan Smith", "skills": [skills[0], skills[5], skills[9]]});
+    this.staffList.push({"name": "Kyle Gruman", "skills": [skills[13], skills[15] ]});
+
+  }
 }
